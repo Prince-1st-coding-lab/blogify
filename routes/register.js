3662f7b7de5ad654868c972f365e78b440bbda69
Prince-1st-coding-lab <princeonedaywill@gmail.com>
2026-01-21 07:58:59 +0200

    5th commit

--- routes/register.js
+++ routes/register.js
@@ -2,17 +2,23 @@ var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcrypt');
 const db = require('./db_connection')
+router.use(express.json());
 
 /* GET registration page. */
 router.get('/', function(req, res, next) {
   res.render('registration');
 });
 
-router.post('/', function(req, res, next) {
+router.post('/',async function (req, res, next) {
   const {full_name,username,email,password,role} = req.body;
-  db.query('INSERT INTO users(fullName,username,email,password,role) VALUES (?,?,?,?,?)',[full_name,username,email,password,role],(err,rows)=>{
-    if(err) return res.send(err);
-    res.send(rows);
+  if(!req.body) return res.json({msg:'no body data sent to server'});
+  
+  const saltRound = 10;
+  const password_hash = await bcrypt.hash(password,saltRound);
+
+  db.query('INSERT INTO users(fullName,username,email,password,role) VALUES (?,?,?,?,?)',[full_name,username,email,password_hash,role], (err,rows)=>{
+    if(err) return res.json({msg:err.sqlMessage});
+    res.json({msg:'user registered successfully'});
   })
 
 });
