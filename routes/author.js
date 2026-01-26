767a68cf1a1a00a3fcfad2170293b9f257b05702
Prince-1st-coding-lab <princeonedaywill@gmail.com>
2026-01-26 07:21:37 +0200

    12nd commit

--- routes/author.js
+++ routes/author.js
@@ -14,20 +14,44 @@ router.get('/',auth_token, function(req, res, next) {
 /* GET all author page. */
 router.get('/all',auth_token, function(req, res, next) {
    if(req.user.role != 'author') return res.json({msg:'you are not author'})
-    db.query('select title,status,updated_at,user_id from posts',(err,rows)=>{
+    db.query('select id,title,status,updated_at,user_id from posts',(err,rows)=>{
        if(err) return res.json({msg:err.sqlMessage});
        res.json(rows)
   })
+});
+////////////////////////////////////////////////////////
+router.get('/delete',auth_token, function(req, res, next) {
+      if(req.user.role != 'author') return res.json({msg:'you are not author'});
+      if(!req.query) return res.json({msg:'no id provided'});
+      const {id} = req.query;
+      db.query('DELETE FROM posts WHERE posts.id = ?',[id],(err,rows)=>{
+    if(err) return res.json({msg:err.sqlMessage});
+    res.json({msg:'Post deleted'})
+   })
+      
+});
+////////////////////////////////////////////////////////
+router.get('/update',auth_token, function(req, res, next) {
+      if(req.user.role != 'author') return res.json({msg:'you are not author'});
+      if(!req.query) return res.json({msg:'no id provided'});
+      const {postId} = req.query;
+      const {title,slug,content,image,category,status} = req.body;
+
+      db.query('UPDATE posts set title=?,slug=?,content=?,featured_image,category=?,status=? WHERE posts.id = ?',[title,slug,content,image,category,status,postId],(err,rows)=>{
+    if(err) return res.json({msg:err.sqlMessage});
+    res.json({msg:'Post updated'})
+   })
+      
 });
 
 ////////////////////////////////////////////////////////////
 router.post('/new/post',auth_token,function(req,res,next){
   if(req.user.role != 'author') return res.json({msg:'you are not author'})
-   const {post_title,slug,post_image,post_category,editor} = req.body;
+   const {post_title,slug,post_image,post_category,editor,status} = req.body;
 
    if (!req.body) return res.status(404).json({msg:'no data sent'});
    
-   db.query('INSERT INTO posts (user_id, title,slug,content,featured_image,category) VALUES (?,?,?,?,?,?)',[req.user.id,post_title,slug,editor,post_image,post_category],(err,rows)=>{
+   db.query('INSERT INTO posts (user_id, title,slug,content,featured_image,category,status) VALUES (?,?,?,?,?,?,?)',[req.user.id,post_title,slug,editor,post_image,post_category,status],(err,rows)=>{
     if(err) return res.json({msg:err.sqlMessage});
     res.json({msg:'new post published'})
    })
