333a92f376c5c189388fc1fd618130d273a62cae
Prince-1st-coding-lab <princeonedaywill@gmail.com>
2026-01-26 20:21:00 +0200

    13rd commit

--- routes/author.js
+++ routes/author.js
@@ -14,7 +14,7 @@ router.get('/',auth_token, function(req, res, next) {
 /* GET all author page. */
 router.get('/all',auth_token, function(req, res, next) {
    if(req.user.role != 'author') return res.json({msg:'you are not author'})
-    db.query('select id,title,status,updated_at,user_id from posts',(err,rows)=>{
+    db.query('select id,title,status,updated_at,user_id from posts where user_id=?',[req.user.id],(err,rows)=>{
        if(err) return res.json({msg:err.sqlMessage});
        res.json(rows)
   })
@@ -31,15 +31,26 @@ router.get('/delete',auth_token, function(req, res, next) {
       
 });
 ////////////////////////////////////////////////////////
-router.get('/update',auth_token, function(req, res, next) {
+router.post('/update',auth_token, function(req, res, next) {
       if(req.user.role != 'author') return res.json({msg:'you are not author'});
       if(!req.query) return res.json({msg:'no id provided'});
       const {postId} = req.query;
       const {title,slug,content,image,category,status} = req.body;
 
-      db.query('UPDATE posts set title=?,slug=?,content=?,featured_image,category=?,status=? WHERE posts.id = ?',[title,slug,content,image,category,status,postId],(err,rows)=>{
+      db.query('UPDATE posts set title=?,slug=?,content=?,featured_image=?,category=?,status=? WHERE posts.id = ?',[title,slug,content,image,category,status,postId],(err,rows)=>{
     if(err) return res.json({msg:err.sqlMessage});
     res.json({msg:'Post updated'})
+   })
+      
+});
+router.get('/all/update',auth_token, function(req, res, next) {
+      if(req.user.role != 'author') return res.json({msg:'you are not author'});
+      if(!req.query) return res.json({msg:'no id provided'});
+      const {postId} = req.query;
+
+      db.query('select * from posts WHERE id = ?',[postId],(err,rows)=>{
+    if(err) return res.json({msg:err.sqlMessage});
+    res.json(rows)
    })
       
 });
