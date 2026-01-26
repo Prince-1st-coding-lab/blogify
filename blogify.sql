0f07f08fba185e795293ccab5238b883985ae4e8
Prince-1st-coding-lab <princeonedaywill@gmail.com>
2026-01-26 07:48:06 +0200

    added post table

--- blogify.sql
+++ blogify.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 20, 2026 at 03:53 PM
+-- Generation Time: Jan 26, 2026 at 06:23 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,37 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `content` longtext NOT NULL,
+  `featured_image` varchar(255) DEFAULT NULL,
+  `category` enum('technology','programming','business','education','lifestyle','news') NOT NULL,
+  `status` enum('draft','pending','published') DEFAULT 'draft',
+  `views` int(11) DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `user_id`, `title`, `slug`, `content`, `featured_image`, `category`, `status`, `views`, `created_at`, `updated_at`) VALUES
+(7, 1, 'title 3', 'title-3', 'xnxxnn', 'image', '', 'draft', 0, '2026-01-25 11:08:22', '2026-01-25 11:08:22'),
+(8, 1, 'tttt', 'tttt', 'kjytre', 'image', '', 'draft', 0, '2026-01-25 11:13:41', '2026-01-25 11:13:41'),
+(9, 1, 'hello mother fucker', 'hello-mother-fucker', 'text', 'image', '', 'published', 0, '2026-01-25 11:17:51', '2026-01-25 11:17:51'),
+(10, 1, 'hello mother fucker 2', 'hello-mother-fucker-2', 'tttttttttttttttt', 'image', '', 'published', 0, '2026-01-25 11:20:09', '2026-01-25 11:20:09'),
+(13, 1, 'tttth', 'tttth', 'hhhhhhhhhhhh', 'imagej ', '', 'draft', 0, '2026-01-25 11:21:29', '2026-01-25 11:21:29');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -37,8 +68,23 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fullName`, `username`, `email`, `password`, `role`) VALUES
+(1, 'Prince', 'prince2k7', 'imbugazange1@yahoo.com', '$2b$10$SjMk9naoPn9vLl/HHPlRN.lABvz1XT0xcPD9Vrl6/ZtFgY9qBr1TS', 'author');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `slug` (`slug`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -53,10 +99,26 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
