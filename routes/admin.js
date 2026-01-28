235c8ca6e94ad8bdf58e5c3c1060c68d19cffbdb
Prince-1st-coding-lab <princeonedaywill@gmail.com>
2026-01-28 12:44:48 +0200

    15th commit

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const auth_token = require('./token_verify')
+const db = require('./db_connection')
 
 router.get('/',auth_token,(req,res,next)=>{
     if(req.user.role == 'admin') return res.render('admin')
     // res.json({msg : 'not admin'})
 res.redirect(`/${req.user.role}`)
 })
+
+
+/////////////////////////////////////////
+router.get('/dashboard',auth_token,(req,res,next)=>{
+    if(req.user.role != 'admin') res.redirect(`/${req.user.role}`)
+   db.query('select posts.* , users.fullName from posts inner join users on posts.user_id = users.id order by updated_at desc',(err,rows)=>{
+       if(err) return res.json({msg:err.sqlMessage});
+       res.json(rows)})
+
+})
+////////////////////////////
 router.post('/logout',(req,res)=>{
   res.clearCookie('token',{
     httpOnly:true,
