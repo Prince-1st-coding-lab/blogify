9f993a925f11acd4ada8cecfa7e9281854fbd767
Prince-1st-coding-lab <princeonedaywill@gmail.com>
2026-02-05 17:28:09 +0200

    20th commit

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/all/posts', function(req, res, next) {
-    db.query('select posts.* , users.id from users inner join posts on posts.user_id = users.id  order by updated_at desc',(err,rows)=>{
+    db.query('select posts.* , users.fullname from users inner join posts on posts.user_id = users.id  order by updated_at desc',(err,rows)=>{
        if(err) return res.json({msg:err.sqlMessage});
        res.json(rows)
       })
+});
+router.get('/post/:slug', function(req, res, next) {
+  if(!req.params.slug) return res.json({msg:'no post title provided'});
+
+  db.query('select posts.* , users.fullName from users inner join posts on  posts.user_id = users.id where posts.slug=?',[req.params.slug],(err,rows)=>{
+       if(err) return res.json({msg:err.sqlMessage});
+       const postDATA = rows[0];
+       res.render('reader.ejs',{
+        title:postDATA.title,
+        authorName:postDATA.fullName,
+        imageURL:postDATA.featured_image,
+        content:postDATA.content,
+        time:postDATA.updated_at,
+        category:postDATA.category
+       })
+       
+      })
+    
 });
 
 module.exports = router;
